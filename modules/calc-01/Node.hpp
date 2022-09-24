0da71c215a979b97cf8ad663e6a0142b85e6e49f
Tilir <konstantin.vladimirov@gmail.com>
2022-09-24 21:55:26 +0300

    Adding simplest non-module calc

---
+++ modules/calc-01/Node.hpp
@@ -0,0 +1,160 @@
+//-----------------------------------------------------------------------------
+//
+// Source code for MIPT masters course on C++
+// Slides: https://sourceforge.net/projects/cpp-lects-rus
+// Licensed after GNU GPL v3
+//
+//------------------------------------------------------------------------------
+//
+// Node.hpp -- concrete node types
+//
+//------------------------------------------------------------------------------
+
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <typeinfo>
+#include <vector>
+
+#include "INode.hpp"
+
+namespace Node {
+
+// Integer value
+class Value final : public INode {
+  int Val_;
+
+  // INode interface
+public:
+  int calc() override { return Val_; }
+  void dump() const override {
+    std::cout << "Node Value: " << Val_ << std::endl;
+  }
+
+public:
+  Value(int V) : Val_(V) {}
+};
+
+// Declaration
+class Decl final : public INode {
+  int Val_;
+  std::string Varname_;
+
+  // INode interface
+public:
+  Decl(const std::string &Name) : Varname_(Name) {}
+  int calc() override { return Val_; }
+  void dump() const override {
+    std::cout << "Node Decl: " << Val_ << std::endl;
+  }
+
+public:
+  Decl() = default;
+  void SetValue(int Val) { Val_ = Val; }
+};
+
+// Scope
+class Scope final : public IScope {
+  std::vector<INode *> Branches_;
+  IScope *PrevScope_;
+
+  // INode interface
+public:
+  int calc() override {
+    for (auto *B : Branches_)
+      B->calc();
+    return 0;
+  }
+
+  void dump() const override {
+    std::cout << "Node Scope: " << std::endl;
+    for (const auto *X : Branches_)
+      X->dump();
+  }
+
+  // IScope interface
+public:
+  IScope *push() { return new Scope(this); }
+  IScope *resetScope() const override { return PrevScope_; }
+  void addBranch(INode *Branch) override { Branches_.push_back(Branch); }
+  INode *access(const std::string &VarName) override;
+  INode *visible(const std::string &VarName) const override;
+
+public:
+  Scope(Scope *PrevScope) : PrevScope_(PrevScope) {}
+  ~Scope();
+};
+
+// Operand
+class Operand final : public INode {
+  INode *Left_;
+  INode *Right_;
+  Operation Op_;
+
+  // INode interface
+public:
+  int calc() override;
+  void dump() const override;
+
+public:
+  Operand(INode *Left, Operation Op, INode *Right)
+      : Left_(Left), Right_(Right), Op_(Op) {}
+  ~Operand();
+};
+
+// While loop node
+class While final : public INode {
+  INode *Op_ = nullptr;
+  INode *Scope_ = nullptr;
+
+  // INode interface
+public:
+  int calc() override {
+    while (Op_->calc())
+      Scope_->calc();
+    return 0;
+  }
+  void dump() const override {
+    std::cout << "Node While " << std::endl;
+    Scope_->dump();
+  }
+
+public:
+  While(INode *Op, INode *Scope) : Op_(Op), Scope_(Scope) {}
+  ~While() {
+    // we can not delete decl here
+    if (Op_ && typeid(Decl) != typeid(*Op_))
+      delete Op_;
+    delete Scope_;
+  }
+};
+
+// If node
+class If final : public INode {
+  INode *Op_;
+  INode *Scope_;
+
+  // INode interface
+public:
+  int calc() override {
+    if (Op_->calc())
+      Scope_->calc();
+    return 0;
+  }
+  void dump() const override {
+    std::cout << "Node If " << std::endl;
+    Scope_->dump();
+  }
+
+public:
+  If(INode *Op, INode *Scope) : Op_(Op), Scope_(Scope) {}
+  ~If() {
+    if (Op_ && typeid(Decl) != typeid(*Op_))
+      delete Op_;
+    delete Scope_;
+  }
+};
+
+} // namespace Node
---
+++ modules/calc-01/driver.cpp
@@ -0,0 +1,63 @@
+//-----------------------------------------------------------------------------
+//
+// Source code for MIPT masters course on C++
+// Slides: https://sourceforge.net/projects/cpp-lects-rus
+// Licensed after GNU GPL v3
+//
+//------------------------------------------------------------------------------
+//
+// driver.cpp -- main entry point
+//
+//------------------------------------------------------------------------------
+
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "parser.hpp"
+
+Node::IScope *CurrentScope = nullptr;
+static int CurrentInlinePos = 0;
+
+FILE *OpenFile(const char *Name) {
+  FILE *F = fopen(Name, "r");
+  if (!F) {
+    perror("Cannot open file");
+    throw std::runtime_error("No input file exists");
+  }
+  return F;
+}
+
+void PrintError(char const *Errmsg) {
+  std::cerr << "Error: " << Errmsg << " - Line " << yylineno << ", Column "
+            << CurrentInlinePos << std::endl;
+  throw std::runtime_error("parse error");
+}
+
+void BeginToken(char *t, int *yyinlinePos) {
+  yylloc.first_line = yylineno;
+  yylloc.first_column = *yyinlinePos;
+  yylloc.last_line = yylineno;
+  *yyinlinePos += strlen(t);
+  yylloc.last_column = *yyinlinePos;
+  CurrentInlinePos = *yyinlinePos;
+}
+
+int main(int argc, char **argv) try {
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " file.pcl" << std::endl;
+    return 0;
+  }
+  auto fdeleter = [](FILE *f) { fclose(f); };
+  std::unique_ptr<FILE, decltype(fdeleter)> F(OpenFile(argv[1]), fdeleter);
+  yyin = F.get();
+  std::unique_ptr<Node::IScope> CurrentScopeOwner{createScope()};
+  CurrentScope = CurrentScopeOwner.get();  
+  yyparse();
+} catch (const std::exception &e) {
+  std::cerr << "Exception: " << e.what() << std::endl;
+} catch (...) {
+  std::cerr << "Exception unknown\n";
+}
